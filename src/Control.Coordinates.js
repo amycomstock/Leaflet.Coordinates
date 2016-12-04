1d8b9dd1426639eb3dcc9dcf7a3ba4934eaaef2d
Amy Comstock <amy.comstock@gmail.com>
2016-12-04 10:02:29 -0500

    Fix issue #22 - Marker disappears when clicked on, previous markers disappear when create a new one

--- src/Control.Coordinates.js
+++ src/Control.Coordinates.js
@@ -208,7 +208,7 @@ L.Control.Coordinates = L.Control.extend({
 
 				var container = L.DomUtil.create("div", "");
 				var label = L.DomUtil.create("div", "", container);
-				label.innerHTML = this._ordinateLabel(ll);
+				label.innerHTML = this._createCoordinateLabel(ll);
 
 				var close = L.DomUtil.create("a", "", container);
 				close.innerHTML = "Remove";
@@ -289,3 +289,60 @@ L.Map.addInitHook(function() {
 		this.addControl(this.coordinateControl);
 	}
 });
+L.NumberFormatter = {
+	round: function(num, dec, sep) {
+		var res = L.Util.formatNum(num, dec) + "",
+			numbers = res.split(".");
+		if (numbers[1]) {
+			var d = dec - numbers[1].length;
+			for (; d > 0; d--) {
+				numbers[1] += "0";
+			}
+			res = numbers.join(sep || ".");
+		}
+		return res;
+	},
+
+	toDMS: function(deg) {
+		var d = Math.floor(Math.abs(deg));
+		var minfloat = (Math.abs(deg) - d) * 60;
+		var m = Math.floor(minfloat);
+		var secfloat = (minfloat - m) * 60;
+		var s = Math.round(secfloat);
+		if (s == 60) {
+			m++;
+			s = "00";
+		}
+		if (m == 60) {
+			d++;
+			m = "00";
+		}
+		if (s < 10) {
+			s = "0" + s;
+		}
+		if (m < 10) {
+			m = "0" + m;
+		}
+		var dir = "";
+		if (deg < 0) {
+			dir = "-";
+		}
+		return ("" + dir + d + "&deg; " + m + "' " + s + "''");
+	},
+
+	createValidNumber: function(num, sep) {
+		if (num && num.length > 0) {
+			var numbers = num.split(sep || ".");
+			try {
+				var numRes = Number(numbers.join("."));
+				if (isNaN(numRes)) {
+					return undefined;
+				}
+				return numRes;
+			} catch (e) {
+				return undefined;
+			}
+		}
+		return undefined;
+	}
+};
